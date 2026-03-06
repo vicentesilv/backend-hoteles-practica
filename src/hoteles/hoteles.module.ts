db2bf12a24dbb860d183d7fd9321dd2cae04de69
Alberto <luigitonto@hotmail.com>
2026-03-06 10:55:22 -0700

    Todo funcionando y probado

--- src/hoteles/hoteles.module.ts
+++ src/hoteles/hoteles.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hotel } from './hotel.entity';
 import { Habitacion } from './habitaciones.entity';
+import { HotelesController } from './hoteles.controller';
+import { HotelesService } from './hoteles.service';
 
 @Module({
-    imports :[TypeOrmModule.forFeature([Hotel]),TypeOrmModule.forFeature([Habitacion])],
+imports: [TypeOrmModule.forFeature([Hotel, Habitacion])],
+controllers: [HotelesController],
+providers: [HotelesService],
+// exports: [HotelesService],
 
 })
 export class HotelesModule {
 }
+
--- src/hoteles/hoteles.service.ts
+++ src/hoteles/hoteles.service.ts
@@ -1,17 +1,42 @@
-import { Injectable } from '@nestjs/common';
-import { CreateHabitacionDTO } from './dto/create-habitacion.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateHotelDto } from './dto/create-hotel.dto';
+import { Hotel } from './hotel.entity'; 
 import { InjectRepository } from '@nestjs/typeorm';
-import { Habitacion } from './habitaciones.entity';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
+import { User } from 'src/user/user.entity';
 
 @Injectable()
 export class HotelesService {
+
   constructor(
-    @InjectRepository(Habitacion)
-    private habitacionRepository: Repository<Habitacion>
-  )
-  function registrarHabitacion(request: CreateHabitacionDTO) {
-  this.habitacionRepository.save()
-}
+          @InjectRepository(Hotel) private readonly hotelRepo: Repository<Hotel>,
+      ){}
+
+  async findOneById(id: number): Promise<Hotel | null> {
+       return this.hotelRepo.findOne({ where: { id } });
+  }
+
+  async createHotel(dto: CreateHotelDto): Promise<Hotel> {
+      const hotel = this.hotelRepo.create(dto);
+      return this.hotelRepo.save(hotel);
+  }
+
+  async updateHotel(id: number, email: string, idHotelero: User, nombre: string, direccion: string, telefono: string): Promise<Hotel> {
+          const exists = await this.hotelRepo.exist({ where: { id } });
+          if (!exists) throw new NotFoundException('Hotel no encontrado');
+          const res = await this.hotelRepo.update({ id }, { idHotelero, email, nombre, direccion, telefono });
+          if (!res.affected) throw new NotFoundException('Sin cambios');
+  
+          const updated = await this.hotelRepo.findOne({ where: { id } });
+          if (!updated) throw new NotFoundException('Hotel no encontrado');
+      return updated;
+      }
+
+// - idhotelero
+// - nombre
+// - direccion
+// - telefono
+// - email
 
 }
+
