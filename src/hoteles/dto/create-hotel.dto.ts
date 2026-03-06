cd4bd289a38783b63f00a4cdd761f31621ab950b
Alberto <luigitonto@hotmail.com>
2026-03-06 07:17:01 -0700

    No he probado el funcionamiento

---
+++ src/hoteles/dto/create-hotel.dto.ts
@@ -0,0 +1,70 @@
+import { Min,IsEmail, IsString, MinLength, IsNumber, IsInt, IsNotEmpty, IsAlpha, IsNumberString, Length} from 'class-validator';
+import { User } from 'src/user/user.entity';
+import { Type } from 'class-transformer';
+
+export class GetHotelDto {
+  @IsNumber()
+  id: number;
+}
+
+export class IdParamDto {
+  @Type(() => Number)
+  @IsInt()
+  @Min(1)
+  id: number;
+}
+
+export class UpdateHotelDto {
+
+  @IsInt({message: 'El id nuevo debe ser un numero entero'})
+  @IsNotEmpty({message: 'El id nuevo no puede estar vacio'})
+  idHotelero: User;
+
+  @IsAlpha('es-ES',{message: 'El nombre nuevo debe contener solo caracteres alfabeticos'})
+  @MinLength(3, {message: 'El nombre nuevo debe ser mayor a $constraint1 caracteres'})
+  nombre: string;
+
+  @IsString()
+  @MinLength(6, {message: 'La direccion nueva debe ser mayor a $constraint1 caracteres'})
+  direccion: string;
+
+  @IsNumberString({},{message: 'El telefono nuevo debe ser un numero'})
+  @Length(10,10, {message: 'El telefono nuevo debe tener exactamente $constraint1 caracteres'})
+  telefono: string;
+  
+  @IsEmail({}, {message: 'El email nuevo debe ser un correo valido'})
+  email: string;
+
+}
+// - idhotelero
+// - nombre
+// - direccion
+// - telefono
+// - email
+
+export class CreateHotelDto {
+  
+  @IsInt({message: 'El id debe ser un numero entero'})
+  @IsNotEmpty({message: 'El id no puede estar vacio'})
+  idHotelero: User;
+
+  @IsAlpha('es-ES',{message: 'El nombre debe contener solo caracteres alfabeticos'})
+  @MinLength(3, {message: 'El nombre debe ser mayor a $constraint1 caracteres'})
+  nombre: string;
+
+  @IsString()
+  @MinLength(6, {message: 'La direccion debe ser mayor a $constraint1 caracteres'})
+  direccion: string;
+
+  @IsNumberString({},{message: 'El telefono debe ser un numero'})
+  @Length(10,10, {message: 'El telefono debe tener exactamente $constraint1 caracteres'})
+  telefono: string;
+  
+  @IsEmail({}, {message: 'El email debe ser un correo valido'})
+  email: string;
+
+  @IsString()
+  foto : string;
+
+
+}
