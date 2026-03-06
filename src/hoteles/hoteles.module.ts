5737314b7403fa3344edcb8cac9da776327d0814
Alberto <luigitonto@hotmail.com>
2026-03-06 07:18:02 -0700

    En teoria funcionando

--- src/hoteles/hoteles.module.ts
+++ src/hoteles/hoteles.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Hotel } from './hotel.entity';
 import { Habitacion } from './habitaciones.entity';
+import { HotelesController } from './hoteles.controller';
+import { HotelesService } from './hoteles.service';
 
 @Module({
     imports :[TypeOrmModule.forFeature([Hotel]),TypeOrmModule.forFeature([Habitacion])],
+    controllers: [HotelesController],
+    providers: [HotelesService]
 
 })
 export class HotelesModule {
