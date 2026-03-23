328012359e1891fdc83f12dfaeab3f5607ea3257
Mario Astorga Hernandez <astorgamhdez@gmail.com>
2026-03-22 18:06:54 -0700

    feat: add delete for habitacion endpoint

--- src/hoteles/hoteles.service.ts
+++ src/hoteles/hoteles.service.ts
@@ -5,17 +5,19 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/user/user.entity';
 import { Habitacion } from './habitaciones.entity';
-import { CreateHabitacionDto, UpdateHabitacionDto } from './dto/habitaciones.dto';
+import {
+  CreateHabitacionDto,
+  UpdateHabitacionDto,
+} from './dto/habitaciones.dto';
 
 @Injectable()
 export class HotelesService {
-
   constructor(
     @InjectRepository(Hotel)
     private readonly hotelRepo: Repository<Hotel>,
     @InjectRepository(Habitacion)
-    private readonly habitacionRepository: Repository<Habitacion>
-  ) { }
+    private readonly habitacionRepository: Repository<Habitacion>,
+  ) {}
 
   async findOneById(id: number): Promise<Hotel | null> {
     return this.hotelRepo.findOne({ where: { id } });
@@ -26,10 +28,20 @@ export class HotelesService {
     return this.hotelRepo.save(hotel);
   }
 
-  async updateHotel(id: number, email: string, idHotelero: User, nombre: string, direccion: string, telefono: string): Promise<Hotel> {
+  async updateHotel(
+    id: number,
+    email: string,
+    idHotelero: User,
+    nombre: string,
+    direccion: string,
+    telefono: string,
+  ): Promise<Hotel> {
     const exists = await this.hotelRepo.exist({ where: { id } });
     if (!exists) throw new NotFoundException('Hotel no encontrado');
-    const res = await this.hotelRepo.update({ id }, { idHotelero, email, nombre, direccion, telefono });
+    const res = await this.hotelRepo.update(
+      { id },
+      { idHotelero, email, nombre, direccion, telefono },
+    );
     if (!res.affected) throw new NotFoundException('Sin cambios');
     const updated = await this.hotelRepo.findOne({ where: { id } });
     if (!updated) throw new NotFoundException('Hotel no encontrado');
@@ -42,7 +54,7 @@ export class HotelesService {
   }
 
   async createHabitacion(request: CreateHabitacionDto): Promise<Habitacion> {
-    const hotel = await this.hotelRepo.findOneBy({id: request.idHotel});
+    const hotel = await this.hotelRepo.findOneBy({ id: request.idHotel });
     if (!hotel) {
       throw new NotFoundException('El hotel no existe');
     }
@@ -53,22 +65,24 @@ export class HotelesService {
       tipo: request.tipo,
       capacidad: request.capacidad,
       precio: request.precio,
-      descripcion: request.descripcion
+      descripcion: request.descripcion,
     });
     return this.habitacionRepository.save(habitacion);
   }
 
-  async updateHabitacion(request: UpdateHabitacionDto, id: number){
+  async updateHabitacion(request: UpdateHabitacionDto, id: number) {
     const habitacion = await this.habitacionRepository.findOne({
       where: { id },
       relations: { idHotel: true },
     });
-    if (!habitacion){
+    if (!habitacion) {
       throw new NotFoundException('La habitacion no existe');
     }
-    let hotelAsignado = habitacion.idHotel
+    let hotelAsignado = habitacion.idHotel;
     if (request.idHotel != null) {
-      const hotelFromHabitacion = await this.hotelRepo.findOneBy({ id: request.idHotel });
+      const hotelFromHabitacion = await this.hotelRepo.findOneBy({
+        id: request.idHotel,
+      });
       if (!hotelFromHabitacion) {
         throw new NotFoundException('El hotel no existe');
       }
@@ -79,11 +93,18 @@ export class HotelesService {
       numHabitacion: request.numHabitacion ?? habitacion.numHabitacion,
       tipo: request.tipo ?? habitacion.tipo,
       capacidad: request.capacidad ?? habitacion.capacidad,
-      estado: request.estado ?? habitacion.estado, 
+      estado: request.estado ?? habitacion.estado,
       precio: request.precio ?? habitacion.precio,
       descripcion: request.descripcion ?? habitacion.descripcion,
     });
-    return this.habitacionRepository.save(updatedHabitacion)
+    return this.habitacionRepository.save(updatedHabitacion);
+  }
+
+  async deleteHabitacion(id: number) {
+    const habitacion = await this.habitacionRepository.findOneBy({ id: id });
+    if (!habitacion) {
+      throw new NotFoundException('La habitacion no existe');
+    }
+    await this.habitacionRepository.delete(habitacion.id);
   }
 }
-
